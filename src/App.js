4b2fb75e23f444ee309660805632ce2f3269ea41
Jonathan Nunez <jonadavid13@gmail.com>
2023-07-22 02:45:58 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sistema web en construcción.
+        </p>
+        <a
+          className="App-link"
+          href="https://jonadavid-webdesign.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sigan viendo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
