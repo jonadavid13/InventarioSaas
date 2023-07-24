c30b5a0930d5ed40b5ddafee76328f70c8045f77
Jonathan <jonadavid13@gmail.com>
2023-07-23 21:44:17 -0400

    prueba upload

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sistema web en construcción.
-        </p>
-        <a
-          className="App-link"
-          href="https://jonadavid-webdesign.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sigan viendo
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Sistema web en construcción.
+          </p>
+          <a
+            className="App-link"
+            href="https://jonadavid-webdesign.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sigan viendo
+          </a>
+        <button name='boton' className='btn btn-secondary disabled mt-4'>Botón desactivado</button>
+        </header>
+      </div>
+    </>
   );
 }
 
